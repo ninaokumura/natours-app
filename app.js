e0c28d4e40aff53898febe95b2b67c5f8eb969eb
Nina Okumura <elaineokumura@gmail.com>
2021-11-09 15:21:20 +1300

    Implement users routes

--- app.js
+++ app.js
@@ -108,6 +108,39 @@ const deleteTour = (req, res) => {
   });
 };
 
+// User handlers
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
 /*
 // Handling get requests -> Get all tours
 app.get('/api/v1/tours', getAllTours);
@@ -134,6 +167,14 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // Starting server
 const port = 3000;
