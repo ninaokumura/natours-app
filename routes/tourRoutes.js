bd3bd8a16c9efc89f60f31d4522db2223e8f4e10
Nina Okumura <elaineokumura@gmail.com>
2021-11-09 16:21:09 +1300

    implement param middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,8 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router(); // middleware
 
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
