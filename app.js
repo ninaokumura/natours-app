cb614d5803a0bdc5ddc23559013b1ea3b74e0a0d
Nina Okumura <elaineokumura@gmail.com>
2021-11-09 13:41:41 +1300

    handling delete requests

--- app.js
+++ app.js
@@ -95,6 +95,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// Handling delete requests
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 // Starting server
 const port = 3000;
 app.listen(port, () => {
