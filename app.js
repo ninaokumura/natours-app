be2f995785576cdab9d7fc4b9e87a2ae003e58b6
Nina Okumura <elaineokumura@gmail.com>
2021-11-09 13:07:51 +1300

    handling POST requests

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
 // Define routes
 /*
 app.get('/', (req, res) => {
@@ -11,7 +14,6 @@ app.get('/', (req, res) => {
     .status(200)
     .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
-
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
 });
@@ -22,7 +24,7 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// Get all tours
+// Handling get requests -> Get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -31,6 +33,29 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+// Handling post requests -> Create a new tour
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  // Persist data
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 // Starting server
