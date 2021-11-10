099ac8c66f6ed429256e1ef6f4b5f58341308a0d
Nina Okumura <elaineokumura@gmail.com>
2021-11-11 10:54:39 +1300

    implement filter feature

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,24 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // The 3 dots will basically take all the fields out of the object
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // This is gonna return a query and we can chain methods
+    const query = Tour.find(queryObj);
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
