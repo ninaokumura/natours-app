6b3e480c008e7d86e150fb23d2928d22233ef3b8
Nina Okumura <elaineokumura@gmail.com>
2021-11-10 17:43:44 +1300

    connect to database

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to remote database
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log('DB conection successful!'));
 
 const app = require('./app');
 
