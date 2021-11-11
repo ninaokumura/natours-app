209e1976cea08886f22d615f1b6bdfcf2333ac68
Nina Okumura <elaineokumura@gmail.com>
2021-11-12 08:03:50 +1300

    add query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,14 +55,14 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
-    toJSON: {
-      virtuals: true,
-    },
-    toObject: {
-      virtuals: true,
-    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
 
@@ -88,6 +88,21 @@ tourSchema.post('save', (doc, next) => {
   next();
 });
 */
+
+// Query middleware
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  console.log(docs);
+  next();
+});
 
 // Model
 const Tour = mongoose.model('Tour', tourSchema);
