927fcebe1866f9ff094fa4c82c9f0cba56ecf098
Nina Okumura <elaineokumura@gmail.com>
2021-11-15 12:44:22 +1300

    Handle unhandled promise rejections

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ mongoose
 
 // Starting server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Handling unhandled rejections
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
