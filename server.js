603314df8d1478c6ef10387d8dd5dcd136970a3c
Nina Okumura <elaineokumura@gmail.com>
2021-11-15 12:56:27 +1300

    Catch uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Catching uncaught esceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -26,8 +33,8 @@ const server = app.listen(port, () => {
 
 // Handling unhandled rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
