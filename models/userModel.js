2d05b1552973d6fa0bc85e4b247bd9165c0cf2b7
Nina Okumura <elaineokumura@gmail.com>
2021-11-15 17:02:43 +1300

    Implement user login

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -45,6 +46,13 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// Instance method
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 // Model variables use Capital letter
 const User = mongoose.model('User', userSchema);
 
