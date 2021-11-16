06aa4f85d178de3f9e3e981d2e0fc570642b4493
Nina Okumura <elaineokumura@gmail.com>
2021-11-16 15:09:19 +1300

    Implement reset token

--- controllers/authController.js
+++ controllers/authController.js
@@ -111,3 +111,19 @@ exports.restrictTo =
     }
     next();
   };
+
+// Reset password
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with this email address.', 404));
+  }
+  // 2) Generate the random token -> created an instance method on userModel.js
+  const resetToken = user.createPasswordResetToken();
+  // The validateBeforeSave will deactivate all the validaters that we specified in our schema.
+  await user.save({ validateBeforeSave: false });
+  // 3) Send it back to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
