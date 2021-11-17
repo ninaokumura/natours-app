3d1c63b65fdbebaba5063b0dcf090ba3c0aff167
Nina Okumura <elaineokumura@gmail.com>
2021-11-18 11:07:15 +1300

    Implement geospatial data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,9 +20,7 @@ mongoose
   .then(() => console.log('DB conection successful!'));
 
 // Read JSON file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // Import data into database
 const importData = async () => {
@@ -51,5 +49,3 @@ if (process.argv[2] === '--import') {
 } else if (process.argv[2] === '--delete') {
   deleteData();
 }
-
-console.log(process.argv);
--- models/tourModel.js
+++ models/tourModel.js
@@ -78,6 +78,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    // We always need to specify an array of objs, this will then create brand new docs insde of the parent document, in this case, the tour.
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
