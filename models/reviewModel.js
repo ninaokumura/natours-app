17fbac72eabb8067b2a89c6814bee648029f3a71
Nina Okumura <elaineokumura@gmail.com>
2021-11-18 12:26:28 +1300

    Model reviews(Parent referencing)

---
+++ models/reviewModel.js
@@ -0,0 +1,42 @@
+// review / rating / createdAt / ref to the tour / ref to the user
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      defaulrt: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// Virtual properties
+
+// Create the model
+const review = mongoose.model('Review', reviewSchema);
+
+module.exports = review;
