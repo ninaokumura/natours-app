1d029dd3d27df4cd7dc056893c489c951ed9940a
Nina Okumura <elaineokumura@gmail.com>
2021-11-19 17:16:35 +1300

    Implement calcAverageRatings on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,6 @@
 // review / rating / createdAt / ref to the tour / ref to the user
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -53,12 +54,54 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  // Aggregation pipeline to create some statistics
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+// Here we should use the post instead of the pre save middleware because at this point it's not really saved yet snd it wont be able to appear in the output. Post cannot get access to next
+reviewSchema.post('save', function () {
+  // console.log(this);
+  // This points to current review
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+// FindByIdAndUpdate and findByIsAndDelete only have query middleware and not document middleware
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.rev = await this.findOne();
+  // console.log(this.rev);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does not work here, query has already executed
+  await this.rev.constructor.calcAverageRatings(this.rev.tour);
+});
+
 // Create the model
-const review = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
-module.exports = review;
-
-// POST / tour / 234d4/ reviews
-// GET / tour / 234d4/ reviews
-
-// Implement nested routes
+module.exports = Review;
