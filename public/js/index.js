f03558329dc58e43d7d6cf88461ec6f873a7b5b3
Nina Okumura <elaineokumura@gmail.com>
2021-11-24 10:27:25 +1300

    Implement logout feature

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,13 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login } from './login';
+import { logout } from './login';
 
 document.addEventListener('DOMContentLoaded', () => {
   // DOM ELEMENTS
   const mapBox = document.getElementById('map');
   const loginForm = document.querySelector('.form');
+  const logoutBtn = document.querySelector('.nav__el--logout');
 
   // DELEGATION
   if (mapBox) {
@@ -20,8 +22,8 @@ document.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
       const email = document.getElementById('email').value;
       const password = document.getElementById('password').value;
-
       login(email, password);
     });
   }
+  if (logoutBtn) logoutBtn.addEventListener('click', logout);
 });
