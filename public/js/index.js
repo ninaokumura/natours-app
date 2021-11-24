7b1d77eb0afe2231d6876b93007eb5bd73be37a2
Nina Okumura <elaineokumura@gmail.com>
2021-11-24 17:31:42 +1300

    Update user password with the API

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 document.addEventListener('DOMContentLoaded', () => {
   // DOM ELEMENTS
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.querySelector('.form--login');
   const logoutBtn = document.querySelector('.nav__el--logout');
   const userDataForm = document.querySelector('.form-user-data');
+  const userPasswordForm = document.querySelector('.form-user-password');
 
   // DELEGATION
   if (mapBox) {
@@ -33,6 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
       const name = document.getElementById('name').value;
       const email = document.getElementById('email').value;
-      updateData(name, email);
+      updateSettings({ name, email }, 'data');
+    });
+
+  if (userPasswordForm)
+    userPasswordForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+      document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+      const passwordCurrent = document.getElementById('password-current').value;
+      const password = document.getElementById('password').value;
+      const passwordConfirm = document.getElementById('password-confirm').value;
+
+      await updateSettings(
+        { passwordCurrent, password, passwordConfirm },
+        'password'
+      );
+
+      document.querySelector('.btn--save-password').textContent =
+        'Save password';
+      document.getElementById('password-current').value = '';
+      document.getElementById('password-current').value = '';
+      document.getElementById('password-confirm').value = '';
     });
 });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -3,22 +3,24 @@ import axios from 'axios';
 import { showAlert } from './alert';
 // updateData and call in index.js
 
-export const updateData = async (name, email) => {
+// type is either 'passqword' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? './api/v1/users/updateMyPassword'
+        : './api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: './api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
   } catch (err) {
-    console.log(err);
     showAlert('error', err.response.data.message);
   }
 };
