fa78c20fe679d4d5da8d0581c85b74e0d05ec365
Nina Okumura <elaineokumura@gmail.com>
2021-11-25 13:05:11 +1300

    Process multiple images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,10 +28,36 @@ exports.uploadTourImages = upload.fields([
 // If we have only one field which accepts multiple images we could do like this:
 // upload.array('images', 5);
 
-exports.resizeTourImages = (req, res, next) => {
-  console.log(req.files);
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+  // PROCESSING COVER IMAGE
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.images[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, idx) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${idx + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
   next();
-};
+});
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
